71d20118085d1637baeec56cb23d5236749eee35
Naveenkumar <113662606+naveenkumar402@users.noreply.github.com>
2023-12-29 10:47:55 +0530

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -29,10 +29,8 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         //config.setAllowCredentials(true);
 
-        //config.addAllowedOrigin("http://localhost:3000");
-        //  config.addAllowedOrigin("https://talent-stream-front-end-version2-kwrsb2zt1-eedekarunakar.vercel.app");
-           config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
-// config.addAllowedOrigin("https://talent-stream-lalitha12-web.vercel.app");
+        config.addAllowedOrigin("http://localhost:3000");
+        
      
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
